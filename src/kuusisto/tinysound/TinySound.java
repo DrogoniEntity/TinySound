25468fa110dcf3cccd752d9767527fd46c719a1f
DrogoniEntity <alexandre.thery@free.fr>
2021-11-26 19:32:48 +0100

    Change version name.

--- src/kuusisto/tinysound/TinySound.java
+++ src/kuusisto/tinysound/TinySound.java
@@ -65,7 +65,7 @@ import kuusisto.tinysound.internal.UpdateRunner;
  */
 public class TinySound {
 	
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.1.1-fork";
 
 	/**
 	 * The internal format used by TinySound.
